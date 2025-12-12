2459db7fff0177a1a1315ebfb3322e222e9dc6bc
Asif Mohtesham <asifmohtesham@gmail.com>
2025-12-12 20:06:16 +0400

    Fixed: Injected dependencies in `StockEntryFormBinding`
    Added: Injected `StockEntryProvider`, `PosUploadProvider`, and `StorageService`
    Added: Dependency injection for `StockEntryFormController`

--- lib/app/modules/stock_entry/form/stock_entry_form_binding.dart
+++ lib/app/modules/stock_entry/form/stock_entry_form_binding.dart
@@ -1,11 +1,18 @@
 import 'package:get/get.dart';
 import 'package:multimax/app/modules/stock_entry/form/stock_entry_form_controller.dart';
+import 'package:multimax/app/data/providers/stock_entry_provider.dart';
+import 'package:multimax/app/data/providers/pos_upload_provider.dart';
+import 'package:multimax/app/data/services/storage_service.dart';
 
 class StockEntryFormBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<StockEntryFormController>(
-      () => StockEntryFormController(),
-    );
+    // Inject dependencies required by StockEntryFormController
+    Get.lazyPut<StockEntryProvider>(() => StockEntryProvider());
+    Get.lazyPut<PosUploadProvider>(() => PosUploadProvider());
+    Get.lazyPut<StorageService>(() => StorageService());
+
+    // Inject the Controller
+    Get.lazyPut<StockEntryFormController>(() => StockEntryFormController());
   }
 }
