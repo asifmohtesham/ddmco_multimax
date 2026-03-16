2efb9166bff8fc2c454fad026dddd1893444230b
Asif Mohtesham <asifmohtesham@gmail.com>
2026-03-16 13:57:59 +0400

    Comment out scanBar in GlobalItemFormSheet

--- lib/app/modules/global_widgets/global_item_form_sheet.dart
+++ lib/app/modules/global_widgets/global_item_form_sheet.dart
@@ -466,7 +466,7 @@ class GlobalItemFormSheet extends StatelessWidget {
                 ),
               ),
             ),
-            if (scanBar != null) scanBar,
+            // if (scanBar != null) scanBar,
           ],
         ),
       );
@@ -489,7 +489,7 @@ class GlobalItemFormSheet extends StatelessWidget {
                 children: _formChildren(context),
               ),
             ),
-            if (scanBar != null) scanBar,
+            // if (scanBar != null) scanBar,
           ],
         ),
       );
