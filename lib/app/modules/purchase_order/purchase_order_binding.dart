450f36dd0d90be6741d687de941a149e2ac4d8a8
Asif Mohtesham <asifmohtesham@gmail.com>
2026-03-23 11:43:17 +0400

    PO: register User, Warehouse and Api providers in binding

--- lib/app/modules/purchase_order/purchase_order_binding.dart
+++ lib/app/modules/purchase_order/purchase_order_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 import 'package:multimax/app/data/providers/purchase_order_provider.dart';
 import 'package:multimax/app/data/providers/supplier_provider.dart';
+import 'package:multimax/app/data/providers/user_provider.dart';
+import 'package:multimax/app/data/providers/warehouse_provider.dart';
+import 'package:multimax/app/data/providers/api_provider.dart';
 import 'package:multimax/app/modules/purchase_order/purchase_order_controller.dart';
 
 class PurchaseOrderBinding extends Bindings {
@@ -8,6 +11,9 @@ class PurchaseOrderBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<SupplierProvider>(() => SupplierProvider());
     Get.lazyPut<PurchaseOrderProvider>(() => PurchaseOrderProvider());
+    Get.lazyPut<UserProvider>(() => UserProvider());
+    Get.lazyPut<WarehouseProvider>(() => WarehouseProvider());
+    Get.lazyPut<ApiProvider>(() => ApiProvider());
     Get.lazyPut<PurchaseOrderController>(() => PurchaseOrderController());
   }
 }
