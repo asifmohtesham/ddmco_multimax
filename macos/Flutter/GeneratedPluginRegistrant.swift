c04c3da284a0fee457ac59fcd34720cc2adfa858
Asif Mohtesham <asifmohtesham@gmail.com>
2026-04-03 19:49:41 +0400

    Add `mobile_scanner` dependency (#12345)
    - Add `mobile_scanner` to `pubspec.yaml` and update `pubspec.lock`.
    - Register `MobileScannerPlugin` in `macos/Flutter/GeneratedPluginRegistrant.swift`.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import mobile_scanner
 import package_info_plus
 import path_provider_foundation
 import share_plus
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
